ae5648f6081c07495e1737bca1a96e031351cb93
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-02-25 04:40:11 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,12 @@
+chance_of_rain = 1000
+puts "Let's go outside!"
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+if (chance_of_rain > 1000)
+  "Evacuate immediately"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "Pack an umbrella!"
+else 
+  puts "On second thought, let's stay home and smoke weed!"
+end
 # Write your solution here
